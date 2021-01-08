6f1dc1afeeeba90dd32c26c442801b9ee8d29582
Michael Wanyoike <micwanyoike@gmail.com>
2021-01-08 13:20:23 +0300

    - Installed TailwindCSS - Implemented Simple Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Header() {
+  return (
+    <header className="p-4 bg-yellow-500">
+      <h1 className="text-4xl font-semibold">Daily Video Chat</h1>
+    </header>
+  )
+}
+
+export default Header
